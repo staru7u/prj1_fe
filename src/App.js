49239ca3d9063bed0f249779378789509f84d84a
hsb <staru7u@naver.com>
2024-10-23 13:55:50 +0900

    게시판 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'; 
+import ListBoardComponent from './components/ListBoardComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+        <HeaderComponent/>
+          <div className="container">
+          <BrowserRouter>
+            <Routes>
+              <Route path = "/"  element = {<ListBoardComponent/>}></Route>
+              <Route path = "/board" element = {<ListBoardComponent/>}></Route>
+            </Routes>
+            </BrowserRouter>
+          </div>
+        <FooterComponent/>
     </div>
   );
 }
